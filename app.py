753ed6e4a64898f371da522ea55ad8d85a158eba
rendonir21@gmail.com <rendonir21@gmail.com>
2025-01-24 15:21:28 -0800

    added list with dictionaries for 3 citites

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import requests
 import json 
 import os 
 
-lat = 35.6895
-lon = 139.6917
+cities = [
+    {"name": "San Francisco", "lat": 37.7337, "lon": -122.4312},
+    {"name": "San Diego", "lat": 32.7153, "lon": -117.1572},
+    {"name": "New York", "lat": 40.7128, "lon": -74.0060}
+]
+
 API_KEY = os.environ.get("SECRET_KEY")
 
 
 app = Flask(__name__)
 app.api_key = os.environ.get("SECRET_KEY")
-
-response = requests.get("https://api.openweathermap.org/data/2.5/weather",
-    params={"lat": lat, "lon": lon, "appid": API_KEY},
-    headers={
-        "Host": "api.openweathermap.org"
-    }
-)
+for city in cities:
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather",
+        params={"lat": city["lat"], "lon": city["lon"], "appid": API_KEY},
+        headers={
+            "Host": "api.openweathermap.org"
+        }
+    )
 
 print(type(response.json()))
 print(response.json())
